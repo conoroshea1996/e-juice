6a2dc5d4c33c787c979e8711b85bbbe2591b3497
conoroshea1996 <conoshea12@gmail.com>
2019-11-12 15:18:07 +0000

    basic navbar using bootstrap setup

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../media/Logo.png';
+import './Navbar.css';
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-function Navbar() {
+function MainNav() {
     return (
-        <React.Fragment>
-            <h3>Hello from Nav</h3>
-        </React.Fragment>
+        <Navbar bg="light" expand="md">
+            <LinkContainer to='/'>
+                <Navbar.Brand>
+                    <img src={Logo} alt='Home' className='LogoImage' />
+                </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
-export default Navbar;
+export default MainNav;
