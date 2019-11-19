d6af82d44915025ba1dce05f69e9f8d107976056
conoroshea1996 <conoshea12@gmail.com>
2019-11-19 10:23:49 +0000

    remove unused imports

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import Logo from '../../media/Logo.png';
 import './Navbar.css';
 import { Navbar, Nav } from 'react-bootstrap';
