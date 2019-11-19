43e731928ef40cb8e132551b820598a59cd78171
conoroshea1996 <conoshea12@gmail.com>
2019-11-19 17:09:03 +0000

    updated look of details page

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -1,31 +1,30 @@
 import React, { useContext } from 'react';
 import ProductContext from '../Context';
-import { Card } from 'react-bootstrap';
+import './detail.css';
 
 
 function Details() {
     const { detail } = useContext(ProductContext);
-    const { img, title, price, inCart } = detail;
+    const { img, title, price, inCart, info } = detail;
     return (
         <React.Fragment>
-            <Card className='m-1' >
-                <Card.Body>
-                    <Card.Img variant='top' height={300} src={img} alt={title} />
-                    <Card.Title>{title}</Card.Title>
-                    {/* <Card.Text>
-                            {info}
-                        </Card.Text> */}
-                    <Card.Footer className='d-flex justify-content-around'>
-                        <Card.Text>
-                            ${price}
-                        </Card.Text>
-                        <Card.Text>
-                            {inCart ? 'add to cart' : 'already in cart'}
-                        </Card.Text>
-                    </Card.Footer>
-                </Card.Body>
-            </Card>
-        </React.Fragment>
+            <div class="cards">
+                <nav>
+
+                </nav>
+                <div class="photo">
+                    <img src={img} />
+                </div>
+                <div class="description">
+                    <h2>{title}</h2>
+                    <h4>Popular House Plant</h4>
+                    <h1>${price}</h1>
+                    <p>{info}</p>
+                    <button>Add to Cart</button>
+                    <button>Wishlist</button>
+                </div>
+            </div >
+        </React.Fragment >
     );
 }
 
